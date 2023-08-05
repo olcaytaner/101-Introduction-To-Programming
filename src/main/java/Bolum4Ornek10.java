38fa375c184692bab9e25a26bf0d13c04a9fe6f9
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2023-08-05 19:23:37 +0300

    Added 5 examples for nested loops.

---
+++ src/main/java/Bolum4Ornek10.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Bolum4Ornek10 {
+
+    public static void main(String[] args){
+        int rowCount, columnCount;
+        Scanner input = new Scanner(System.in);
+        do{
+            System.out.println("Enter number of rows: ");
+            rowCount = input.nextInt();
+        }while (rowCount < 2);
+        do{
+            System.out.println("Enter number of columns: ");
+            columnCount = input.nextInt();
+        }while (columnCount % 2 == 0 || columnCount < 3);
+        for (int i = 1; i <= rowCount; i++){
+            if (i == 1){
+                for (int j = 1; j <= columnCount; j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            } else {
+                for (int j = 1; j <= columnCount / 2; j++){
+                    System.out.print(" ");
+                }
+                System.out.println("*");
+            }
+        }
+    }
+
+}
---
+++ src/main/java/Bolum4Ornek11.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Bolum4Ornek11 {
+
+    public static void main(String[] args){
+        int rowCount;
+        Scanner input = new Scanner(System.in);
+        do{
+            System.out.println("Enter number of rows: ");
+            rowCount = input.nextInt();
+        }while (rowCount % 2 == 0 || rowCount < 3);
+        for (int i = 1; i <= rowCount; i++){
+            if (i == rowCount / 2 + 1){
+                System.out.println("**");
+            } else {
+                if (i < rowCount / 2 + 1){
+                    System.out.print("*");
+                    for (int j = 1; j <= rowCount / 2 - i + 1; j++){
+                        System.out.print(" ");
+                    }
+                    System.out.println("*");
+                } else {
+                    System.out.print("*");
+                    for (int j = 1; j <= i - rowCount / 2 - 1; j++){
+                        System.out.print(" ");
+                    }
+                    System.out.println("*");
+                }
+            }
+        }
+    }
+
+}
